1d3c3905441483d2369237316402d2db2a3887f6
Imax <imax13@mail.ru>
2016-02-02 22:20:38 +0300

    Week 1 complete

--- Week 1/1.Test-Pandas/read_csv_pandas.py
+++ Week 1/1.Test-Pandas/read_csv_pandas.py
@@ -1,20 +1,84 @@
 import pandas
 import numpy as np
-data = pandas.read_csv('titanic.csv', index_col='PassengerId')
-data.head()
-print (data.head())
-sort = data.sort_values(['Sex'], ascending=False)
-sort.head(1)
-print (data['Sex'].value_counts())
-print (data['Survived'].value_counts())
-print (data['Survived'].value_counts()[1]/data['Survived'].count())
 
-X = np.random.normal(loc=1, scale=10, size=(1000, 50))
-# print (X)
-
-print (data['Pclass'].value_counts()[1]/data['Pclass'].count())
+def main():
+    data = readCSV('titanic.csv', indexColumn = 'PassengerId')
+    # print (data.head())
+    quize1(data)
+    quize2(data)
+    quize3(data)
+    quize4(data)
+    quize5(data)
+    quize6(data)
 
 
-print (data['Age'].mean())
+def readCSV(name, indexColumn):
+    data = pandas.read_csv(name, index_col=indexColumn)
+    return data
 
-print (data['Age'].median())
+def quize1(data):
+# Какое количество мужчин и женщин ехало на корабле? В качестве ответа приведите два числа через пробел.
+    print ('--------------------------------------')
+    print ('1.Какое количество мужчин и женщин ехало на корабле? В качестве ответа приведите два числа через пробел.')
+    print (data['Sex'].value_counts())
+    print (data['Sex'].value_counts().get_value(label='male'))
+    print (data['Sex'].value_counts().get_value(label='female'))
+
+def quize2(data):
+# Какой части пассажиров удалось выжить? Посчитайте долю выживших пассажиров. Ответ приведите в процентах (число в интервале от 0 до 100, знак процента не нужен).
+    print ('--------------------------------------')
+    print ('2.Какой части пассажиров удалось выжить? Посчитайте долю выживших пассажиров. '
+           'Ответ приведите в процентах (число в интервале от 0 до 100, знак процента не нужен).')
+    print (data['Survived'].value_counts())
+    print (data['Survived'].value_counts().get_value(label=1)/data['Survived'].count())
+
+
+def quize3(data):
+# Какую долю пассажиры первого класса составляли среди всех пассажиров? Ответ приведите в процентах (число в интервале от 0 до 100, знак процента не нужен).
+    print ('--------------------------------------')
+    print ('3.Какую долю пассажиры первого класса составляли среди всех пассажиров? '
+           'Ответ приведите в процентах (число в интервале от 0 до 100, знак процента не нужен)')
+    print (data['Pclass'].value_counts().get_value(label=1)/data['Pclass'].count())
+
+def quize4(data):
+# Какого возраста были пассажиры? Посчитайте среднее и медиану возраста пассажиров. В качестве ответа приведите два числа через пробел.
+    print ('--------------------------------------')
+    print ('4.Какого возраста были пассажиры? Посчитайте среднее и медиану возраста пассажиров. В качестве ответа приведите два числа через пробел.')
+    print (data['Age'].mean())
+    print (data['Age'].median())
+
+
+def quize5(data):
+# Коррелируют ли число братьев/сестер с числом родителей/детей? Посчитайте корреляцию Пирсона между признаками SibSp и Parch.
+    print ('--------------------------------------')
+    print ('5.Коррелируют ли число братьев/сестер с числом родителей/детей? Посчитайте корреляцию Пирсона между признаками SibSp и Parch.')
+    print (data['SibSp'].corr(data['Parch'], 'pearson'))
+
+
+def quize6(data):
+# Какое самое популярное женское имя на корабле? Извлеките из полного имени пассажира (колонка Name) его личное имя (First Name).
+# Это задание — типичный пример того, с чем сталкивается специалист по анализу данных. Данные очень разнородные и шумные,
+# но из них требуется извлечь необходимую информацию. Попробуйте вручную разобрать несколько значений столбца
+# Name и выработать правило для извлечения имен, а также разделения их на женские и мужские.
+    print ('--------------------------------------')
+    print ('6.Какое самое популярное женское имя на корабле? Извлеките из полного имени пассажира (колонка Name) его личное имя (First Name).')
+    X = data.loc[data['Sex'] == 'female']
+    X1 = X['Name']
+    X2 = X1.apply(parse)
+    # print(X2)
+    X3 = X2.groupby(X2).count()
+    print(X3)
+
+
+def parse(name):
+    parsedName = name.split(".")[1].strip()
+    if parsedName.find("(") >= 0 and parsedName.find(")") >= 0:  # .replace("(", "").replace(")", "").split(" ")[0]
+        parsedName = parsedName.split("(")[1].split(")")[0].split(" ")[0].strip()
+        print(parsedName)
+    else :
+        parsedName = parsedName.split(" ")[0]
+        print(parsedName)
+    return parsedName
+
+
+main()
---
+++ Week 1/2.Decision Trees/decision_trees.py
@@ -0,0 +1,41 @@
+import pandas
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+
+def main():
+    data = readCSV('titanic.csv', indexColumn = 'PassengerId')
+    quize1(data)
+
+
+def readCSV(name, indexColumn):
+    data = pandas.read_csv(name, index_col=indexColumn)
+    return data
+
+def quize1(data):
+# 1.Загрузите выборку из файла titanic.csv с помощью пакета Pandas.
+# 2.Оставьте в выборке четыре признака: класс пассажира (Pclass), цену билета (Fare), возраст пассажира (Age) и его пол (Sex).
+# 3.Обратите внимание, что признак Sex имеет строковые значения.
+# 4.Выделите целевую переменную — она записана в столбце Survived.
+# 5.В данных есть пропущенные значения — например, для некоторых пассажиров неизвестен их возраст.
+# 6.Такие записи при чтении их в pandas принимают значение nan.
+# Найдите все объекты, у которых есть пропущенные признаки, и удалите их из выборки.
+# Обучите решающее дерево с параметром random_state=241 и остальными параметрами по умолчанию.
+# Вычислите важности признаков и найдите два признака с
+# наибольшей важностью. Их названия будут ответами для данной задачи
+# (в качестве ответа укажите названия признаков через запятую или пробел, порядок не важен).
+    dataF = data[['Pclass', 'Fare', 'Age', 'Sex','Survived']]
+    dataF = dataF.dropna()
+    Y = dataF['Survived']
+    dataF = dataF[['Pclass', 'Fare', 'Age', 'Sex']]
+    clf = DecisionTreeClassifier(random_state=241)
+    dataF.loc[dataF['Sex'] != 'male', 'Sex'] = 0
+    dataF.loc[dataF['Sex'] == 'male', 'Sex'] = 1
+    print (dataF)
+    clf.fit(dataF, Y)
+    importances = clf.feature_importances_
+    print(importances)
+    # d = zip(dataF.columns, clf.feature_importanc_)
+    # print(d)
+    return
+
+main()
