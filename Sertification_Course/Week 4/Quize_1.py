12a0ba31b4d54445d7cd8d5c0b6cef8d456bde6b
Imax <imax13@mail.ru>
2016-03-05 00:38:29 +0300

    3 - 4  week ML

---
+++ Sertification_Course/Week 4/Quize_1.py
@@ -0,0 +1,60 @@
+# # -*- coding: utf-8 -*-
+
+# Распределе́ние Паре́то в теории вероятностей — двухпараметрическое семейство абсолютно непрерывных распределений
+# ,являющихся степенными. Называется по имени Вилфредо Парето.
+# Встречается при исследовании различных явлений, в частности, социальных,
+# экономических, физических и других[1].
+# Вне области экономики иногда называется также распределением Брэдфорда.
+# Вилфредо Парето изначально использовал это распределение для описания распределения благосостояния,
+# а также распределения дохода[2]. Его правило 20 к 80 (которое гласит: 20 %
+# популяции владеет 80 % богатства) однако зависит от конкретной величины k, и утверждается,
+# что фактически встречаются существенные количественные отклонения, например, данные
+# самого Парето по Британии в Cours d'économie politique говорят, что там примерно 30 % населения
+# владеет 70 % общего дохода.
+# https://ru.wikipedia.org/wiki/%D0%A0%D0%B0%D1%81%D0%BF%D1%80%D0%B5%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5_%D0%9F%D0%B0%D1%80%D0%B5%D1%82%D0%BE
+# http://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.pareto.html#scipy.stats.pareto
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import pareto
+
+b = 10
+
+# pareto_rv = pareto.stats(b, moments='mvsk')
+# print type(pareto_rv)
+
+ax = plt.subplots(1, 1)
+
+sample = pareto.rvs(b, size=1000)
+
+plt.hist(sample, normed=True, bins=40, alpha=0.8)
+plt.ylabel('F(X)')
+plt.xlabel('$x$')
+
+print type(sample)
+print sample
+
+x = np.linspace(pareto.ppf(0.01, b),  pareto.ppf(0.99, b), 100)
+
+plt.plot(x, pareto.pdf(x, b), 'r-', lw=5, alpha=0.7, label='pareto pdf')
+
+mean, var, skew, kurt = pareto.stats(b, moments='mvsk')
+print pareto.std(b)
+print mean, var, skew, kurt
+
+# для построения ECDF используем библиотеку statsmodels
+# from statsmodels.distributions.empirical_distribution import ECDF
+# ecdf = ECDF(sample)
+# print ecdf.x
+# plt.step(ecdf.x, ecdf.y, label='ECDF')
+
+
+sample = pareto.rvs(b, size=1000)
+
+plt.hist(sample, normed=True, bins=10, alpha=0.8)
+plt.ylabel('F(X)')
+plt.xlabel('$x$')
+
+
+plt.show()
